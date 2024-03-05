70b9b6078687c0081a92c8637d9bb99157a5cf13
mateuszpietrusinski <1325258@gmail.com>
2024-03-05 13:05:48 +0100

    feat: adding styling with tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -31,17 +31,46 @@ function App() {
     }
 
     return (
-        <div className="d-flex">
-            <form onSubmit={handleSubmit}>
-                <input type="text" data-testid="todo-input"/>
-                <button data-testid="add-todo">Add</button>
-            </form>
-            <ul data-testid="list">
-                {todos.map(todo => (
-                    <TodoItem key={todo.id} todo={todo} onChange={handleTodoCompletionChange}
-                              onRemove={handleTodoRemove}/>
-                ))}
-            </ul>
+        <div className="container mx-auto">
+            <div className="relative flex flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md w-96">
+                <div className="p-6">
+                    <h2 className="text-left text-2xl font-bold  text-blue-950">
+                        To-Do list app
+                    </h2>
+                </div>
+                <div className="p-6 pt-0">
+                    <div className=" sm:w-full sm:max-w-sm">
+                        <form onSubmit={handleSubmit} className="flex">
+                            <div className="relative h-11 w-full min-w-[200px]">
+                                <input
+                                    className="peer h-full w-full rounded-md border border-blue-gray-200 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                                    placeholder=" "
+                                    type="text"
+                                    name="todo"
+                                    id="todo"
+                                    data-testid="todo-input"
+                                    required
+                                />
+                                <label
+                                    className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                                    Your To-Do!
+                                </label>
+                            </div>
+                            <button data-testid="add-todo"
+                                    className="ml-1 select-none rounded-lg bg-pink-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                            >
+                                Add
+                            </button>
+                        </form>
+                        <ul data-testid="list">
+                            {todos.map(todo => (
+                                <TodoItem key={todo.id} todo={todo} onChange={handleTodoCompletionChange}
+                                          onRemove={handleTodoRemove}/>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
 
     )
--- src/todos/components/TodoItem.tsx
+++ src/todos/components/TodoItem.tsx
@@ -5,12 +5,46 @@ interface TodoItemProps {
     onChange: (id: number) => void,
     onRemove: (id: number) => void
 }
+
 export default function TodoItem({todo, onChange, onRemove}: TodoItemProps) {
     return (
-        <li data-testid="todo-item">
-            <input data-testid="todo-item-completed" type="checkbox" checked={todo.completed} onChange={() => onChange(todo.id)}/>
-            <span data-testid="todo-item-text" style={todo.completed ? {textDecoration: 'line-through'} : {}}>{todo.value}</span>
-            <button data-testid="todo-item-remove" onClick={() => onRemove(todo.id)}>Remove</button>
+        <li data-testid="todo-item" className="mt-4 border-b-2 py-2 flex justify-between items-center">
+            <div className="inline-flex items-center">
+                <label
+                    className="relative flex cursor-pointer items-center rounded-full p-3"
+                    htmlFor="checkbox-1"
+                    data-ripple-dark="true"
+                >
+                    <input
+                        data-testid="todo-item-completed" type="checkbox" checked={todo.completed}
+                        onChange={() => onChange(todo.id)}
+                        className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-pink-500 checked:bg-pink-500 checked:before:bg-pink-500 hover:before:opacity-10"
+                    />
+                    <div
+                        className="pointer-events-none absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 text-white opacity-0 transition-opacity peer-checked:opacity-100">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-3.5 w-3.5"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            stroke="currentColor"
+                            stroke-width="1"
+                        >
+                            <path
+                                fill-rule="evenodd"
+                                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                                clip-rule="evenodd"
+                            ></path>
+                        </svg>
+                    </div>
+                </label>
+            </div>
+            <span data-testid="todo-item-text"
+                  style={todo.completed ? {textDecoration: 'line-through'} : {}}>{todo.value}</span>
+            <button data-testid="todo-item-remove"
+                    className="ml-1 select-none rounded-lg bg-gray-700 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-500/20 transition-all hover:shadow-lg hover:shadow-gray-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                    onClick={() => onRemove(todo.id)}>Remove
+            </button>
         </li>
     )
 }
